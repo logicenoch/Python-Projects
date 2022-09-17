b3879b01ff5e295aac9326fd53fb24749fdb90ed
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-09-18 00:22:36 +0100

    Minor changes

--- hello.py
+++ hello.py
@@ -1,2 +1,13 @@
-#My first python program
-print("Hello Python");
+# My first python program
+print("Hello Python")
+
+# Variable declarations
+myNumber = 3
+print(myNumber)
+
+myNumber2 = 200
+print(myNumber2)
+
+myNumber2 = "Hello Python"
+print(myNumber2)
+
